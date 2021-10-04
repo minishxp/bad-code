a12a5c552814ec55e8b8bf25873b678dfebc4816
minishxp <miclbratsiankov.6666@gmail.com>
2021-10-04 15:17:45 -0400

    3 files modified, 1 added, 1 deleted

--- js/getStaticFiles.js
+++ js/getStaticFiles.js
@@ -1,21 +1,21 @@
 var __globalData = '';
+var textEditor = document.querySelector('#__EDITOR-MAIN');
 
-function GetFileSrc(filename) {
-  fetch(window.location + '/staticStorage/' + filename).then((response) => {
-    return response;
-  });
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
 
-document.addEventListener('DOMContentLoaded', function (event) {
-  var fileBtns = document.querySelector('#files');
-  var textEditor = document.querySelector('#__EDITOR-MAIN');
 
-  $('#files')
-    .children('input')
-    .each(function () {
-      console.log(this.innerHTML);
-      this.onclick = function () {
-        textEditor.innerHTML = GetFileSrc(this.innerHTML);
-      };
-    });
-});
+var children = document.getElementById('files').getElementsByTagName('*');
+
+for( var i = 0; i<children.length; i++){
+  console.log(window.location)
+    children[i].onclick = function() {
+      textEditor.value = httpGet(window.location + "/staticStorage/" + children[i].innerHTML)
+      console.log('fired')
+    }
+}
