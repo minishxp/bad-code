9d6c5bc403807bda457d2f4f34acc219e2a52f65
minishxp <miclbratsiankov.6666@gmail.com>
2021-10-07 11:03:42 -0400

    2 files modified, 1 added

---
+++ js/notif.js
@@ -0,0 +1,19 @@
+var toggleNotif = null
+var notifState = true
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  toggleNotif = function(text) {
+    if (notifState) {
+      document.getElementById("main-notif").style.display = 'none'
+      document.getElementById("body-wrap").style.filter = 'none'
+      notifState = false
+    }
+    else {
+      document.getElementById("main-notif").style.display = 'inline'
+      document.getElementById("main-notif-txt").innerHTML = text
+      document.getElementById("body-wrap").style.filter = 'blur(5px)'
+      document.getElementById("main-notif").style.filter = 'none'
+      notifState = true
+    }
+  }
+})
