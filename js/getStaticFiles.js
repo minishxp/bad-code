ad7b750eb06e3c09f31ce2bb0f79361eedbb54b4
minishxp <miclbratsiankov.6666@gmail.com>
2021-10-12 12:30:00 -0400

    1 file modified

--- js/getStaticFiles.js
+++ js/getStaticFiles.js
@@ -1,22 +1,53 @@
+const server = 'https://XCodeStaticFileServer.0509798916.repl.co';
+let currentFile = null;
+
+function httpGet(theUrl) {
+  var e = ""
+
+  fetch(theUrl)
+    .then((data) => {
+      console.log(data);
+    })
+    .then((post) => {
+      e = post
+      console.log(post)
+    });
+
+    return e
+}
+
+console.log('[Script/GetStaticFiles] Loading static file functions...');
+function write(file, content) {
+  return httpGet(
+    server +
+      '/api/write?key=69xdreal&file=' +
+      escape(file) +
+      '&newContent=' +
+      escape(content)
+  );
+}
+
+function read(file) {
+  return httpGet(server + '/api/access/files/' + escape(file));
+}
+
+function remove(file) {
+  return httpGet(server + '/api/remove?key=69xdreal&file=' + escape(file));
+}
+
+function add(file) {
+  return httpGet(server + '/api/add?key=69xdreal&file=' + escape(file));
+}
+console.log('[Script/GetStaticFiles] Loaded!');
+
 document.addEventListener('DOMContentLoaded', function (event) {
-  console.log('loaded')
-  var __globalData = '';
-  var textEditor = document.querySelector('#__EDITOR-MAIN');
+  const editor = document.getElementById('__EDITOR-MAIN');
 
-  function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', theUrl, false);
-    xmlHttp.send(null);
-    return xmlHttp.response;
-  }
-
-  var fileBtns = document.getElementsByClassName('fl-btn');
-  fileBtns.length
-
-  for (let item of fileBtns) {
-    item.onclick = function() {
-      textEditor.value = httpGet(window.location + "staticStorage/" + item.innerHTML)
-      console.log('click recieved for ' + window.location + "staticStorage/" + item.innerHTML)
-    }
+  var buttons = document.getElementsByClassName('fl-btn');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+      currentFile = buttons[i].innerHTML;
+      editor.value = read(buttons[i].innerHTML);
+    };
   }
 });
