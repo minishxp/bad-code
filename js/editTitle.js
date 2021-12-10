50d73a0e438542f100bfe2673fd225f1e0e357c8
minishxp <miclbratsiankov.6666@gmail.com>
2021-12-10 07:58:13 -0500

    4 files modified

--- js/editTitle.js
+++ js/editTitle.js
@@ -1,16 +1,34 @@
 let editTitle
-document.addEventListener("DOMContentLoaded", function() {
-  let title = document.getElementById("sidebar-status-txt")
-  let chars = ["A", "B", "C", "1", "2", "3"]
 
-  let rndChars = 
+document.addEventListener("DOMContentLoaded", function() {
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  alphabet.push
+
+
+  function randomNumber(min, max) { 
+    return parseInt((Math.random() * (max - min) + min).toString().split(".")[0]);
+  }
+
+  function rchar() {
+    let chars = ["A", "B", "C", "1", "2", "3"]
+    
+    let rchar = alphabet[randomNumber(0, alphabet.length)]
+    return rchar
+  }
+
+  let title = document.getElementById("sidebar-status-txt")
+
+
   
   editTitle = function(newTitle) {
     let title = document.getElementById("sidebar-status-txt")
     let proh_title = document.getElementById("__settings-api-name-handle")
     title.innerHTML = newTitle
-    proh_title.innerHTML = "minishxp / " + newTitle.replaceAll(" ", "-")
+    proh_title.innerHTML = "minishxp / " + newTitle.replaceAll(" ", "-").toLowerCase()
   }
+
+  editTitle("Test-" + (rchar() + rchar() + rchar() + rchar()))
 
   document.getElementById("rename-btn").onclick = function() {
     let res = prompt("New Name:", document.getElementById("sidebar-status-txt").innerHTML)
--- js/readfile.js
+++ js/readfile.js
@@ -1,4 +1,24 @@
+
+
 let readFile;
+let saveToLocalFile;
+function download(data, filename, type) {
+  var file = new Blob([data], {type: type});
+  if (window.navigator.msSaveOrOpenBlob) // IE10+
+      window.navigator.msSaveOrOpenBlob(file, filename);
+  else { // Others
+      var a = document.createElement("a"),
+              url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);  
+      }, 0); 
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   // I know this is mostly for readfile BUT
@@ -21,20 +41,30 @@ document.addEventListener('DOMContentLoaded', function () {
   check();
 
   readFile = function () {
+    
     var input = document.createElement('input');
     input.type = 'file';
 
     input.onchange = (e) => {
       var file = e.target.files[0];
+      
       var reader = new FileReader();
+      reader.fileName = file.name
       reader.readAsText(file, 'UTF-8');
       reader.onload = (readerEvent) => {
         var content = readerEvent.target.result;
+        
         editor.setValue(content);
         localStorage.setItem("__editor_data__", content)
+        editTitle(readerEvent.target.fileName)
       };
     };
 
     input.click();
   };
+
+  saveToLocalFile = function() {
+    let title = document.getElementById("sidebar-status-txt").innerHTML
+    download(editor.getValue(), title, "txt")
+  }
 });
